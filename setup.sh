78d3ddc6042405dac193c7d87f560ce7f1a20f8b
Mikel <zhobroikel@gmail.com>
2021-03-24 02:46:00 +0100

    Setup check outs master from tools submod of tools-multistep.

--- setup.sh
+++ setup.sh
@@ -9,3 +9,5 @@ echo "Change into tools-multisetup"
 cd tools-multisetup/
 git submodule init
 git submodule update
+git submodule foreach git config credential.helper store
+git submodule foreach git checkout -B master origin/master
