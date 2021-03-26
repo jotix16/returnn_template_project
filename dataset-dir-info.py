a69a793877606f7f647b14d1358d1d56c3711cfe
Mikel <zhobroikel@gmail.com>
2021-03-26 18:51:25 +0100

    update

--- dataset-dir-info.py
+++
@@ -1,6 +0,0 @@
-import os
-
-if os.path.exists("/work"):
-  dataset = "/work/asr3/zeyer/zhobro/setups-data/librispeech/dataset"
-else:
-  dataset = os.path.join(os.environ["HOME"], "datasets/LibriSpeech")
--- setup-data-dir-info.py
+++ setup-data-dir-info.py
@@ -1,7 +1,14 @@
 # i6
 import os
 
+# Workdir path
 if os.path.exists("/work"):
   workdir_base = "/work/asr3/zeyer"
 else:
   workdir_base = "/tmp"
+
+# Dataset path
+if os.path.exists("/work"):
+  dataset = "/work/asr3/zeyer/zhobro/setups-data/librispeech/dataset"
+else:
+  dataset = os.path.join(os.environ["HOME"], "datasets/LibriSpeech")
