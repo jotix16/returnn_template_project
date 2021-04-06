7b7a2fe8b9d0076b439be51aa90f595451cdf201
Mikel <zhobroikel@gmail.com>
2021-04-06 19:22:09 +0200

    update dataset path i6

--- setup-data-dir-info.py
+++ setup-data-dir-info.py
@@ -8,7 +8,7 @@ else:
   workdir_base = "/tmp"
 
 # Dataset path
-if os.path.exists("/work"):
-  dataset = "/work/asr3/zeyer/zhobro/setups-data/librispeech/dataset"
-else:
-  dataset = os.path.join(os.environ["HOME"], "datasets")
+# if os.path.exists("/work"):
+#   dataset = "/work/asr3/zeyer/zhobro/setups-data/librispeech/dataset"
+# else:
+dataset = os.path.join(os.environ["HOME"], "datasets")
