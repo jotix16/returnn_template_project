24fb6a006b482367cacf91c0cf0da1931b58d194
Mikel Zhobro <zhobromikel@gmail.com>
2021-05-18 23:03:38 +0200

    add dump-align path

--- setup-data-dir-info.py
+++ setup-data-dir-info.py
@@ -12,3 +12,9 @@ else:
 #   dataset = "/work/asr3/zeyer/zhobro/setups-data/librispeech/dataset"
 # else:
 dataset = os.path.join(os.environ["HOME"], "datasets")
+
+# Dump-align
+if os.path.exists("/work"):
+  dump_align_data = "/work/asr4/zeyer/setups-data/switchboard/2019-10-22--e2e-bpe1k/dump-align/"
+else:
+  dump_align_data = "/tmp"
